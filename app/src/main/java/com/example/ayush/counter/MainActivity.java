3d12a05aff6d21d4330b15fddf41bf29c9fe819d
AyushDevil <aj96ayushjha@gmail.com>
2017-09-23 20:26:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ayush/counter/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ayush.counter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView showValue;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showValue = (TextView) findViewById(R.id.CountValue);
+    }
+
+    public void count (View view){
+
+        counter++;
+        showValue.setText(Integer.toString(counter));
+    }
+}
